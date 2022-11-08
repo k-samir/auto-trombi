6af498e191848c2a5abf347eb8a2696764d8b086
k-sam <k-samir@live.fr>
2022-11-08 01:52:55 +0100

    Init Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,50 @@ import { Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Index from "./components/Index/Index";
 import Login from "./components/Login/Login";
+import { useEffect } from "react";
+import { themeChange } from "theme-change";
+import { FaGithub } from "react-icons/fa";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/index" element={<Index />} />
-    </Routes>
+    <div className="flex justify-center">
+      <header className="bg-primary h-14 flex  px-28 w-[80%] rounded-b-lg text-lg">
+        <a
+          className="flex self-center flex-1 font-comfortaa text-white"
+          href="/"
+        >
+          auto-trombi
+        </a>
+        <div className="flex flex-1 justify-end gap-8">
+          <div className="flex self-center gap-5">
+            <a
+              href="/"
+              className="border-0 text-white hover:bg-transparent hover:text-white/40 capitalize text-md py-1"
+            >
+              Sign in
+            </a>
+            <a
+              href="/index"
+              className="border-solid text-white border-[1px] rounded-md hover:bg-white/40 hover:border-white capitalize h-fit text-md px-2 py-1 "
+            >
+              Sign up
+            </a>
+          </div>
+          <a
+            className="flex self-center"
+            href="https://github.com/k-samir/auto-trombi"
+          >
+            <FaGithub size={33} color="white" />
+          </a>
+        </div>
+      </header>
+      <div className="text-center justify-center flex centered">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/index" element={<Index />} />
+        </Routes>
+      </div>
+    </div>
   );
 };
 
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,9 +1,64 @@
-import "./Login.scss";
+import { FaLock, FaUser } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import { Input } from 'antd';
 
+import "./Login.scss";
 const Login = () => {
+
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    navigate('/index');
+  };
+
+
   return (
-       <p>Login</p>
+    <div className="rounded-lg bg-white w-fit flex flex-col p-16 pt-12 gap-10">
+      
+        <h1 className="font-comfortaa font-extrabold text-neutral">Sign In</h1>
+
+      <form className="flex flex-col gap-2"  onSubmit={handleSubmit}>
+        <div className="">
+          <label className="input-group input-group-md">
+            <span>
+              <FaUser />
+            </span>
+
+            <input
+            required 
+              type="text"
+              placeholder="Username"
+              className="input input-bordered input-md"
+            />
+          </label>
+        </div>
+
+       
+
+        <div className="">
+          <label className="input-group input-group-md">
+            <span>
+              <FaLock className="flex self-center" />
+            </span>
+
+            <input
+            required 
+              type="password"
+              placeholder="Password"
+              className="input input-bordered input-md"
+            />
+          </label>
+        </div>
+
+        <button className="btn btn-primary" type="submit">Log In</button>
+      </form>
+
+      
+    </div>
   );
 };
 
 export default Login;
+// <Input size="large" placeholder="large size" prefix={ <FaUser />} />
