89675252f8ee99faa216fd07ce270e5f1ea94349
k-sam <k-samir@live.fr>
2022-11-08 01:52:24 +0100

    Adding daisy ui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "comfortaa": ["Comfortaa", "cursive"],
+      },
+    },
   },
-  plugins: [],
-}
+  daisyui: {
+    themes: ["dark","light"],
+  },
+  plugins: [require("daisyui"), require("@tailwindcss/typography")],
+};
