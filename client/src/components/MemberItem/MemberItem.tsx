b889be8b8e20bd9015d6e73e078c4f17f1a687bc
k-sam <k-samir@live.fr>
2022-11-14 22:58:53 +0100

    Update data and fix css

--- client/src/components/MemberItem/MemberItem.tsx
+++ client/src/components/MemberItem/MemberItem.tsx
@@ -6,35 +6,32 @@ type Props = {
 const MemberItem = (props: Props) => {
   const { memberId } = props;
 
-  
-  if(memberId){
+  if (memberId) {
     const member = useGetMember(memberId);
 
+    return (
+      <div className=" rounded-2xl bg-blue-500 items-center w-32 md:w-44 flex flex-col gap-2">
+        <div className="w-28 md:w-[60%]">
+          <img className="mask mask-squircle" src={member?.picture} />
+        </div>
+
+        <div className=" flex flex-col flex-1 place-content-evenly">
+          <div className="flex flex-col text-center">
+            <h3 className="text-black font-semibold">{member?.firstname} </h3>
+            <h3 className="text-black font-medium">{member?.lastname}</h3>
+          </div>
+
+          <div className=" p-2 self-center">
+            <img className="p-2 sm:max-w-[8rem] max-h-[4rem]" src={member?.companyLogo} />
+          </div>
+        </div>
+      </div>
+    );
+  }
   return (
-    <div className="rounded-2xl flex flex-col bg-white w-28 gap-5 items-center p-2">
-      <div className=" flex object-fill w-24">
-        <img className="mask mask-circle" src={member?.picture} />
-      </div>
-
-      <div className="flex flex-1 h-[20%]">
-        <h3 className="text-black">
-          {member?.firstname} {member?.lastname}
-        </h3>
-      </div>
-
-      <div className="flex p-2 h-[40%]">
-        <img className="object-scale-down" src={member?.companyLogo} />
-      </div>
+    <div className="self-center rounded-2xl flex flex-col bg-white w-18 h-18 items-center p-2 justify-center">
+      <HiUserAdd size={44} color="black" />
     </div>
-  );}
-return (
-  <div className="rounded-2xl flex flex-col bg-white w-28 items-center p-2 justify-center">
- 
-
-    <HiUserAdd size={50} color="black"/>
-
-</div>
-)
-
+  );
 };
 export default MemberItem;
